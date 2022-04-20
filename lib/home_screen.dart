5bcde487cf5f27b64e96c594e38b06af3d67841d
kirillkozlovskyi <cucuruza>
2022-04-20 12:38:36 +0300

    create point table widget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,6 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _scoreX = 0;
+  int _scoreO = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +33,67 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       backgroundColor: Colors.grey[900],
+      body: Column(
+        children: [
+          _buildPointTable(),
+          // _buildGrid(),
+          // _buildTurn(),
+
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPointTable() {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const Text(
+                  'Player O', style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400),
+                ),
+               const SizedBox(height: 20),
+                Text(
+                  _scoreO.toString(),
+                  style: const TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const Text(
+                  'Player X', style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  _scoreX.toString(),
+                  style: const TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                )
+
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
